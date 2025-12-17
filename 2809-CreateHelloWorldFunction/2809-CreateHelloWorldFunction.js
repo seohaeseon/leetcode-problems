5875931dc1069bb8f852721fc4f94d173de7cb40
haeseon <pqpqpq460@gmail.com>
2025-12-18 00:05:26 +0900

    Create 2809-CreateHelloWorldFunction

---
+++ 2809-CreateHelloWorldFunction/2809-CreateHelloWorldFunction.js
@@ -0,0 +1,15 @@
+// Last updated: 2025. 12. 18. 오전 12:05:24
+/**
+ * @return {Function}
+ */
+var createHelloWorld = function() {
+    
+    return function(...args) {
+        return "Hello World";
+    }
+};
+
+/**
+ * const f = createHelloWorld();
+ * f(); // "Hello World"
+ */
