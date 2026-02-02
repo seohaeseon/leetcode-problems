164a0c3009ae243a1cd59f2a693dcef393e13dee
haeseon <pqpqpq460@gmail.com>
2026-02-02 15:58:34 +0000

    rename variables

--- 9-PalindromeNumber/version/typescript/9-PalindromeNumber_20260203_002911.ts
+++ 9-PalindromeNumber/version/typescript/9-PalindromeNumber_20260203_002911.ts
@@ -1,15 +1,14 @@
-// Last updated: 2026. 2. 3. 오전 12:29:11
-1const isPalindrome = (x: number): boolean => {
-2    if (x < 0) return false;
-3
-4    const findReversedValue = (remained: number, reversed: number = 0) => {
-5        if (remained < 10) {
-6            return reversed + remained;
-7        };
-8
-9        const onesDigit = remained % 10;
-10        return findReversedValue((remained - onesDigit) / 10, (reversed + onesDigit) * 10);
-11    }
-12
-13    return x === findReversedValue(x) ? true : false;
-14}
+const isPalindrome = (num: number): boolean => {
+    if (num < 0) return false;
+
+    const reverseNumber = (remaining: number, reversed: number = 0) => {
+        if (remaining < 10) {
+            return reversed + remaining;
+        };
+
+        const lastDigit = remaining % 10;
+        return reverseNumber((remaining - lastDigit) / 10, (reversed + lastDigit) * 10);
+    }
+
+    return num === reverseNumber(num) ? true : false;
+}
